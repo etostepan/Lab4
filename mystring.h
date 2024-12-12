f1cf1e498f2b8be6e93f1082465886b470287aa0
korotaev.sa <korotaev.sa@dozen.mephi.ru>
2024-12-13 02:31:58 +0300

    13.12 2:30

--- mystring.h
+++
@@ -1,9 +0,0 @@
-#ifndef MYSTRING_H
-#define MYSTRING_H
-char *strChr(char *, char);
-char *getStr(const char *str);
-char *strDup(const char *str);
-int strLen(const char *str);
-char *strTok(char *s, const char *ct);
-void *memCpy(const char *, const char *, int);
-#endif
